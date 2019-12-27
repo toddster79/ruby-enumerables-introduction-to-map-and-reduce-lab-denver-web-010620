50b9b0571f7209af3ce67b7de9968a0f0096c679
Todd Carlson <todd.carlson3030@me.com>
2019-12-27 21:11:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,4 @@
 # My Code here....
-def map_to_negativize(source_array)
- counter = 0 
- if source_array[counter] < source_array.length do 
-   source_array[counter] * -1
-   counter += 1
- end 
-end 
+
+def map_to_negativize(source_array) 
+  
