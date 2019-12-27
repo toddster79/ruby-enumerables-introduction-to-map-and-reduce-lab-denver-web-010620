8fa890ffd6b785de8dcfbec5cd3a8fee2f408531
Todd Carlson <todd.carlson3030@me.com>
2019-12-27 21:40:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
-  source_array.reduce { |sum, x| sum + x }
+  source_array.reduce(starting_point) { |sum, x| sum + x }
 end 
 
 def reduce_to_all_true(source_array)
