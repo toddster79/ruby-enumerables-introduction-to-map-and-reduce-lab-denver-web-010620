84e3f67ce77bff2a73a48a79a345010487314742
Todd Carlson <todd.carlson3030@me.com>
2019-12-27 21:43:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,7 +16,7 @@ def map_to_square(source_array)
   source_array.map { |x| x * x } 
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   source_array.reduce(starting_point) { |sum, x| sum + x }
 end 
 
