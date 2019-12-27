514f4682573f626e76bcd2f9f51af0d0414ac246
Todd Carlson <todd.carlson3030@me.com>
2019-12-27 21:41:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,6 +18,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point) { |sum, x| sum + x }
+  source_array.reduce { |sum, x| sum + x }
 end 
 
 def reduce_to_all_true(source_array)
